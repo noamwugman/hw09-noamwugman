2e24e2facd11633ad7f658aa1013d88ae644aa88
noamwugman <noamwug24@gmail.com>
2026-02-16 21:15:47 +0200

    noam

--- LanguageModel.java
+++ LanguageModel.java
@@ -33,20 +33,62 @@ public class LanguageModel {
 
     /** Builds a language model from the text in the given file (the corpus). */
 	public void train(String fileName) {
-		// Your code goes here
-	}
+        String window = "";
+        char c;
+        In in = new In(fileName);
+
+        for (int i = 0; i < windowLength && !in.isEmpty(); i++) {
+            window += in.readChar();
+        }
+        
+        while (!in.isEmpty()) {
+            c = in.readChar();
+            List probs = CharDataMap.get(window);
+            if (probs == null) {
+                probs = new List();
+                CharDataMap.put(window, probs);
+            }
+            probs.update(c);
+            window = window.substring(1) + c;
+        }
+        
+        for (List probs : CharDataMap.values()) {
+            calculateProbabilities(probs);
+        }
+    }
 
     // Computes and sets the probabilities (p and cp fields) of all the
 	// characters in the given list. */
-	void calculateProbabilities(List probs) {				
-		// Your code goes here
-	}
+	void calculateProbabilities(List probs) {
+        int totalCount = 0;
+        ListIterator iter = probs.listIterator(0);
+        while (iter.hasNext()) {
+            CharData cd = iter.next();
+            totalCount += cd.count;
+        }
+        
+        double cumulativeProb = 0.0;
+        iter = probs.listIterator(0);
+        while (iter.hasNext()) {
+            CharData cd = iter.next();
+            cd.p = (double) cd.count / totalCount;
+            cumulativeProb += cd.p;
+            cd.cp = cumulativeProb;
+        }
+    }
 
     // Returns a random character from the given probabilities list.
 	char getRandomChar(List probs) {
-		// Your code goes here
-		return ' ';
-	}
+        double r = randomGenerator.nextDouble();
+        ListIterator iter = probs.listIterator(0);
+        while (iter.hasNext()) {
+            CharData cd = iter.next();
+            if (cd.cp > r) {
+                return cd.chr;
+            }
+        }
+        return probs.get(probs.getSize() - 1).chr;
+    }
 
     /**
 	 * Generates a random text, based on the probabilities that were learned during training. 
@@ -56,9 +98,23 @@ public class LanguageModel {
 	 * @return the generated text
 	 */
 	public String generate(String initialText, int textLength) {
-		// Your code goes here
-        return "";
-	}
+        if (initialText.length() < windowLength) {
+            return initialText;
+        }
+        String generatedText = initialText;
+        String window = initialText.substring(initialText.length() - windowLength);
+        while (generatedText.length() < textLength) {
+            List probs = CharDataMap.get(window);
+            if (probs == null) {
+                break;
+            }
+            char c = getRandomChar(probs);
+            generatedText += c;            
+            window = generatedText.substring(generatedText.length() - windowLength);
+        }
+        
+        return generatedText;
+    }
 
     /** Returns a string representing the map of this language model. */
 	public String toString() {
@@ -71,6 +127,23 @@ public class LanguageModel {
 	}
 
     public static void main(String[] args) {
-		// Your code goes here
+        int windowLength = Integer.parseInt(args[0]);
+        String initialText = args[1];
+        int generatedTextLength = Integer.parseInt(args[2]);
+        Boolean randomGeneration = args[3].equals("random");
+        String fileName = args[4];
+        
+        // Create the LanguageModel object
+        LanguageModel lm;
+        if (randomGeneration)
+            lm = new LanguageModel(windowLength);
+        else
+            lm = new LanguageModel(windowLength, 20);
+        
+        // Trains the model, creating the map.
+        lm.train(fileName);
+        
+        // Generates text, and prints it.
+        System.out.println(lm.generate(initialText, generatedTextLength));
     }
 }
--- List.java
+++ List.java
@@ -30,20 +30,37 @@ public class List {
 
     /** GIVE Adds a CharData object with the given character to the beginning of this list. */
     public void addFirst(char chr) {
-        // Your code goes here
+        Node newFirst = new Node(new CharData(chr), first);
+        first = newFirst;
+        size++;
     }
     
     /** GIVE Textual representation of this list. */
     public String toString() {
-        // Your code goes here
-        return "";
+        String str = "(";
+        Node current = this.first;
+        while (current != null) {
+            str += current.toString();
+            if (current.next != null) {
+                str += " ";
+            }
+            current = current.next;
+        }
+        return str + ")";
     }
 
     /** Returns the index of the first CharData object in this list
      *  that has the same chr value as the given char,
      *  or -1 if there is no such object in this list. */
     public int indexOf(char chr) {
-        // Your code goes here
+        Node current = this.first;
+        int counter = 0;
+        while (current != null) {
+            if (current.cp.equals(chr))
+                return counter;
+            current = current.next;
+            counter++;
+        }
         return -1;
     }
 
@@ -51,14 +68,35 @@ public class List {
      *  increments its counter. Otherwise, adds a new CharData object with the
      *  given chr to the beginning of this list. */
     public void update(char chr) {
-        // Your code goes here
+        if (indexOf(chr) == -1)
+            addFirst(chr);
+        else {
+            Node current = this.first;
+            while (!current.cp.equals(chr)) {
+                current = current.next;
+            }
+            current.cp.count++;
+        }
     }
 
     /** GIVE If the given character exists in one of the CharData objects
      *  in this list, removes this CharData object from the list and returns
      *  true. Otherwise, returns false. */
     public boolean remove(char chr) {
-        // Your code goes here
+        if (indexOf(chr) != -1) {
+            if (this.first.cp.equals(chr)) {
+                this.first = this.first.next;
+                size--;
+                return true;
+            }
+            Node current = this.first;
+            while (!current.next.cp.equals(chr)) {
+                current = current.next;
+            }
+            current.next = current.next.next;
+            size--;
+            return true;
+        }
         return false;
     }
 
@@ -66,8 +104,13 @@ public class List {
      *  If the index is negative or is greater than the size of this list, 
      *  throws an IndexOutOfBoundsException. */
     public CharData get(int index) {
-        // Your code goes here
-        return null;
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException();
+        Node current = this.first;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.cp;
     }
 
     /** Returns an array of CharData objects, containing all the CharData objects in this list. */
